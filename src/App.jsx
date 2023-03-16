4d46d95b0b2331c71d4e5c6da04b9f2ecfadb2e7
Masa Orel <orel.masha89@gmail.com>
2023-03-16 15:52:31 +0100

    added an object for each num and nanoid for key

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import "./App.css";
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   const [allDice, setAllDice] = React.useState(allNewDice());
-
+  console.log(allDice);
   function allNewDice() {
     const array = [];
     for (let i = 0; i < 10; i++) {
-      array.push(Math.ceil(Math.random() * 6));
+      array.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return array;
   }
 
-  function rollDice(){
-    setAllDice(allNewDice())
+  function rollDice() {
+    setAllDice(allNewDice());
   }
-
 
   return (
     <main>
       <div className="inner">
         {allDice.map((dieNum) => (
-          <Die value={dieNum} />
+          <Die value={dieNum.value} key={dieNum.id} />
         ))}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
     </main>
   );
 }
