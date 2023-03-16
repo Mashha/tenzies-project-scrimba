8d2b8d9e4e2a0a0855ea67970e1be53dca65ba67
Masa Orel <orel.masha89@gmail.com>
2023-03-16 14:04:24 +0100

    added die components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Die from "./components/Die";
 
 function App() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="inner">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+  return (
+    <div className="die-div">
+      <h2>{props.value}</h2>
+    </div>
+  );
+}
