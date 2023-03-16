df9a35dd226ed1d755b7ad64ff23a972c94af515
Masa Orel <orel.masha89@gmail.com>
2023-03-16 14:37:45 +0100

    array of 10 random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import "./App.css";
 import Die from "./components/Die";
 
 function App() {
+  function allNewDice() {
+    const array = [];
+    for (let i = 0; i < 10; i++) {
+      array.push(Math.ceil(Math.random() * 6));
+    }
+    return array;
+  }
+  console.log(allNewDice());
+
   return (
     <main>
       <div className="inner">
