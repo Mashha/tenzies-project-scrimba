e99d13802b03673c190ae1e1cc68fd96ed49c092
Masa Orel <orel.masha89@gmail.com>
2023-03-17 10:05:56 +0100

    added title and instructions

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,11 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="inner">
         {allDice.map((dieNum) => (
           <Die
